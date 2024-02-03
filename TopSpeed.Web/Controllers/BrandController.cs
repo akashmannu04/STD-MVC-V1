ea924ae0f57dc3b71132621b8c42ffa8c55533bc
akashmannu04 <143244972+akashmannu04@users.noreply.github.com>
2024-02-03 17:05:33 +0530

    Required Changes done

--- TopSpeed.Web/Controllers/BrandController.cs
+++ TopSpeed.Web/Controllers/BrandController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using TopSpeed.Web.Data;
 using TopSpeed.Web.Models;
 
@@ -45,6 +46,7 @@ namespace TopSpeed.Web.Controllers
             {
                 _dbContext.Brandv2.Add(brandv2);
                 _dbContext.SaveChanges();
+                TempData["success"] = "Record created successfully";
                 return RedirectToAction(nameof(Index));
             }
             return View();
@@ -56,6 +58,84 @@ namespace TopSpeed.Web.Controllers
                                                                                  //If matches the both value.
                                                                                  //It will fetch the details to user 
             return View(brandv2);
+        }
+        [HttpGet]
+        public IActionResult Edit(Guid id)
+        {
+            Brandv2 brandv2 = _dbContext.Brandv2.FirstOrDefault(x => x.Id == id);
+            return View(brandv2);
+        }
+        [HttpPost]
+        public IActionResult Edit(Brandv2 brandv2)
+        {
+            string webRootPath = _webHostEnivironment.WebRootPath; //It helps to access the path
+            var file = HttpContext.Request.Form.Files;//It helps to get image request through form
+            if (file.Count > 0)
+            {
+                string newFileName = Guid.NewGuid().ToString();//It is used to assign name for file
+                var upload = Path.Combine(webRootPath, @"images\brand");//It is used to specify the path to upload a file
+                var extension = Path.GetExtension(file[0].FileName);//It is used to get file extension
+                //delete old image
+                var objFromDb=_dbContext.Brandv2.AsNoTracking().FirstOrDefault(x=>x.Id==brandv2.Id);//It is used to find brand details and we are taking object from DB
+                //AsNoTracking() -> It is used to avoid clash. Because we are taking image from EF core and delete the image. Similar time we are updating the same object 
+                if(objFromDb != null) 
+                {
+                    var oldImagePath = Path.Combine(webRootPath, objFromDb.BrandLogo.Trim('\\'));//We need to find old image path and trims the slash
+                    if(System.IO.File.Exists(oldImagePath))//We need to check file is exist or not
+                    {
+                        System.IO.File.Delete(oldImagePath);//It is used to delete the old image
+                    }
+                
+                }
+
+                using (var fileStream = new FileStream(Path.Combine(upload, newFileName + extension), FileMode.Create)) //It is used to copy the image
+                {
+                    file[0].CopyTo(fileStream); //respective file will copy in the respective folder
+                }
+                brandv2.BrandLogo = @"\images\brand\" + newFileName + extension;// It helps to store the path in the Brand model which we already specified
+            }
+
+            if (ModelState.IsValid)
+            {
+                var objFromDb = _dbContext.Brandv2.AsNoTracking().FirstOrDefault(x => x.Id == brandv2.Id);
+                objFromDb.Name=brandv2.Name;
+                objFromDb.EstablishedYear=brandv2.EstablishedYear;
+                if(brandv2.BrandLogo!=null)
+                {
+                    objFromDb.BrandLogo = brandv2.BrandLogo;
+                }
+
+                _dbContext.Brandv2.Update(objFromDb); 
+                _dbContext.SaveChanges();
+
+                TempData["warning"] = "Record updated successfully";
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+        public IActionResult Delete(Guid id)
+        {
+            Brandv2 brandv2 = _dbContext.Brandv2.FirstOrDefault(x => x.Id == id);
+            return View(brandv2);
+        }
+        [HttpPost]
+        public IActionResult Delete(Brandv2 brandv2) {
+            string webRootPath=_webHostEnivironment.WebRootPath;
+            if (string.IsNullOrEmpty(brandv2.BrandLogo))
+            {
+                var objFromDb=_dbContext.Brandv2.AsNoTracking().FirstOrDefault(x=>x.Id== brandv2.Id);
+                if(objFromDb!=null)
+                {
+                    var oldImagePath=Path.Combine(webRootPath, brandv2.BrandLogo.Trim('\\'));
+                    if(System.IO.File.Exists(oldImagePath))
+                    {
+                        System.IO.File.Delete(oldImagePath);
+                    }
+                }
+            }
+            _dbContext.Brandv2.Remove(brandv2);
+            _dbContext.SaveChanges();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
